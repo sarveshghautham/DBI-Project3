3482298602494d770593ea3ba877817607806e71
Sarvesh Ghautham <sarvesh.kaladhar@gmail.com>
2014-03-28 21:30:15 -0400

    Major bug fix in test.cc for q2()
    Deleting unwanted files.

--- test.cc
+++ test.cc
@@ -129,7 +129,7 @@ void q2 () {
 
 	Attribute att3[] = {IA, SA, DA};
 	Schema out_sch ("out_sch", numAttsOut, att3);
-	int cnt = clear_pipe (_p, p->schema (), true);
+	int cnt = clear_pipe (_out, &out_sch, true);
 
 	cout << "\n\n query2 returned " << cnt << " records \n";
 
